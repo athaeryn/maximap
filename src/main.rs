c1785e8900fd761bfb65c0adc5067ef9aa2d4748
Mike Anderson <mike@mkndrsn.com>
2016-08-22 01:39:51 -0500

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ use sdl2::render::Texture;
 
 struct DisplayObject {
     texture: Texture,
-    rect: Rect
+    rect: Rect,
 }
 
 fn render_with_sdl(maps: &Vec<FileMap>) {
@@ -43,44 +43,46 @@ fn render_with_sdl(maps: &Vec<FileMap>) {
     renderer.set_draw_color(Color::RGB(255, 255, 255));
 
     let mut x_offset = 0;
-    let display_objs = maps.iter().map(|map| {
-        let mut texture =
-            renderer.create_texture_streaming(PixelFormatEnum::RGB24, map.width, map.height)
-                .unwrap();
-        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-            for y in 0..map.height {
-                for x in 0..map.width {
-                    let offset = y as usize * pitch + x as usize * 3;
-                    buffer[offset + 0] = 255;
-                    buffer[offset + 1] = 255;
-                    buffer[offset + 2] = 255;
-                }
-            }
-            for (y, line) in map.lines.iter().enumerate() {
-                for (x, character) in line.characters.iter().enumerate() {
-                    let offset = y as usize * pitch + x as usize * 3;
-                    match character {
-                        &Character::Whitespace => { },
-                        &Character::Normal => {
-                            buffer[offset + 0] = 64;
-                            buffer[offset + 1] = 64;
-                            buffer[offset + 2] = 64;
+    let display_objs = maps.iter()
+        .map(|map| {
+            let mut texture =
+                renderer.create_texture_streaming(PixelFormatEnum::RGB24, map.width, map.height)
+                    .unwrap();
+            texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                    for y in 0..map.height {
+                        for x in 0..map.width {
+                            let offset = y as usize * pitch + x as usize * 3;
+                            buffer[offset + 0] = 255;
+                            buffer[offset + 1] = 255;
+                            buffer[offset + 2] = 255;
                         }
                     }
-                }
+                    for (y, line) in map.lines.iter().enumerate() {
+                        for (x, character) in line.characters.iter().enumerate() {
+                            let offset = y as usize * pitch + x as usize * 3;
+                            match character {
+                                &Character::Whitespace => {}
+                                &Character::Normal => {
+                                    buffer[offset + 0] = 64;
+                                    buffer[offset + 1] = 64;
+                                    buffer[offset + 2] = 64;
+                                }
+                            }
+                        }
+                    }
+                })
+                .unwrap();
+
+            let rect = Rect::new(x_offset as i32, 0, map.width, map.height);
+
+            x_offset += map.width + 10;
+
+            DisplayObject {
+                texture: texture,
+                rect: rect,
             }
         })
-        .unwrap();
-
-        let rect = Rect::new(x_offset as i32, 0, map.width, map.height);
-
-        x_offset += map.width + 10;
-
-        DisplayObject {
-            texture: texture,
-            rect: rect
-        }
-    }).collect::<Vec<_>>();
+        .collect::<Vec<_>>();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
